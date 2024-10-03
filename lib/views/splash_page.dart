95d2f91f56b4a620d3db09ba038a577300ed8e76
foundaway98 <foundaway98@naver.com>
2024-10-03 17:12:06 +0900

    [기능] router 및 splash page 이후 동작  추가

--- lib/views/splash_page.dart
+++ lib/views/splash_page.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/theme/layout/scaffold_layout.dart';
+import 'package:flutter_application_1/providers/auth_provider.dart';
 import 'package:flutter_application_1/widgets/title_with_logo_widget.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends ConsumerStatefulWidget {
   const SplashPage({super.key});
 
   @override
+  SplashPageState createState() => SplashPageState();
+}
+
+class SplashPageState extends ConsumerState<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToNextScreen();
+  }
+
+  _navigateToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 3));
+    if (!mounted) return;
+
+    final isLoggedIn = ref.read(authProvider);
+    if (isLoggedIn) {
+      context.go('/main');
+    } else {
+      context.go('/login');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const ScaffoldLayout(
-      useAppBar: false,
-      useDrawer: false,
-      body: Column(
-        children: [TitleWithLogoWidget()],
-      ),
-    );
+    return Scaffold(
+        body: Column(
+      children: [
+        SizedBox(
+          height: 80.h,
+        ),
+        const TitleWithLogoWidget()
+      ],
+    ));
   }
 }
